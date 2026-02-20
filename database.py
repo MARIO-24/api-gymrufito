613ea2d19d0bbfda6f5cfda257c5b93a0726007a
Mario Sánchez Rufino <mariosanxezr24@gmail.com>
2026-02-20 17:30:27 +0100

    Ignorar el archivo .env y actualizar .gitignore

--- database.py
+++ database.py
@@ -1,11 +1,15 @@
 import pymysql
 from pymysql.cursors import DictCursor
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def get_connection():
     return pymysql.connect(
-        host="localhost",
-        user="root",
-        password="",  # tu contraseña
-        database="gym_rufito",
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
         cursorclass=DictCursor
     )
